f507ed6334be2b525d195ffbf04ab9c35d3e7a7e
SirFrank0 <frankseb04@gmail.com>
2022-12-20 18:52:20 -0500

    .

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
-let miVariable = 24;
+let miVariable = 24;                     //Se asigna valor a let 
    
 
-console.log("mi edad es " + miVariable)
+console.log("mi edad es " + miVariable)         // muestra 24
 
 
-miVariable = 12;
-console.log("mi edad ahora es " + miVariable)
+miVariable = 12;                                    // Se asigna nuevo valor a let 
+console.log("mi edad ahora es " + miVariable)               // cambia su valor , muestra 12
 
 
-const miConstante = 12;                  // const varibale que no cambia su valor aun si se le asigna otro despues
+const miConstante = 3;                  // const varibale que no cambia su valor aun si se le asigna otro despues
 
-console.log("mi edad constante es " + miConstante)
+console.log("mi edad constante es " + miConstante)       // no cambia su valor , muestra 12
 
 
-miConstante = 4;
-console.log("mi edad constante ahora es " + miConstante)
+miConstante = 4;                                // Se asigna nuevo valor a const 
+console.log("mi edad constante ahora es " + miConstante)            // no cambia su valor y da error
 
