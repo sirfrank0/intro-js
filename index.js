352e3c5533b19466acc0139dff1cdd5b7e0de8e8
SirFrank0 <frankseb04@gmail.com>
2022-12-20 18:47:14 -0500

    exa 1

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 
-let numero = 5;
-const numero2 = 6;      //varibale que no cambia su valor
+let miVariable = 24;
+   
+
+console.log("mi edad es " + miVariable)
+
+
+miVariable = 12;
+console.log("mi edad ahora es " + miVariable)
+
+
+const miConstante = 12;                  // const varibale que no cambia su valor aun si se le asigna otro despues
+
+console.log("mi edad constante es " + miConstante)
+
+
+miConstante = 4;
+console.log("mi edad constante ahora es " + miConstante)
 
