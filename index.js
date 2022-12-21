63765981a318e53c1ca8f3ce24f480b4ec2e5ebb
SirFrank0 <frankseb04@gmail.com>
2022-12-20 19:08:19 -0500

    intro condicionales

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
+// VARIABLES
+
 
 let miVariable = 24;                     //Se asigna valor a let 
    
 
-console.log("mi edad es " + miVariable)         // muestra 24
+//console.log("mi edad es " + miVariable)         // muestra 24
 
 
 miVariable = 12;                                    // Se asigna nuevo valor a let 
-console.log("mi edad ahora es " + miVariable)               // cambia su valor , muestra 12
+//console.log("mi edad ahora es " + miVariable)               // cambia su valor , muestra 12
+
+
+
 
 
 const miConstante = 3;                  // const varibale que no cambia su valor aun si se le asigna otro despues
 
-console.log("mi edad constante es " + miConstante)       // no cambia su valor , muestra 12
+//console.log("mi edad constante es " + miConstante)       // no cambia su valor , muestra 12
 
 
-miConstante = 4;                                // Se asigna nuevo valor a const 
-console.log("mi edad constante ahora es " + miConstante)            // no cambia su valor y da error
+//miConstante = 4;                                // Se asigna nuevo valor a const 
+//console.log("mi edad constante ahora es " + miConstante)            // no cambia su valor y da error
 
+
+
+
+//CONDICIONALES
+// Tablas de verdad: = igual a , != distinto a , > mayor a , < menor a,  ><=, mayor o menor a, 
+
+let miNumero = 6
+let miNombre = 'Frank'
+
+
+if (miNombre == 'Frank')    {                         // 1 igual = significa asigancion y dos igulaes == significan comparacion, 3 iguales === significa comparar el tipo de variable, si es texto o numero
+    console.log('Si')
+}else{
+    console.log('No')
+}
+
+
+
+
+if (miNumero < 2 && miNombre == 'Frank')    { 
+    console.log('Si')                        
+}else{
+    console.log('No')
+}
